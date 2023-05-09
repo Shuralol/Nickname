b01533ae558c0fc8dcc64866eb9bab5cfd14777a
Shuralol <akinfeev09@bk.ru>
2023-05-09 14:00:33 +0700

    Nickname 9 05 23

--- src/js/character.js
+++ src/js/character.js
@@ -1,5 +1,7 @@
 export class Validator {
   validateUsername(username) {
+    const myRegExp = /^(?!.*\d{4})/;
+    const usernameLatinDashLowerNumbers = /^[a-zA-Z0-9_-]{3,25}$/;
     const pattern = /^(?!.*\d{4})[a-zA-Z0-9_-]{3,25}$/;
     return !!username.match(pattern);
   }
